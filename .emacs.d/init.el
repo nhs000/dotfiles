219629d78c4400b0af689692bb925cc0effa4888
Son Nguyen <nhs.000.dev@gmail.com>
2022-02-21 20:49:43 +0700

    force download package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "loader.org"
@@ -10,7 +17,8 @@
  '(ansi-color-names-vector
    ["#282a3a" "#ff657a" "#bad761" "#ffd76d" "#9cd1bb" "#9cd1bb" "#9cd1bb" "#eaf2f1"])
  '(custom-safe-themes
-   '("b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default))
+   (quote
+    ("b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default)))
  '(exwm-floating-border-color "#3d3f4d")
  '(fci-rule-color "#595d68")
  '(highlight-tail-colors ((("#363b3d") . 0) (("#333a46") . 20)))
@@ -19,7 +27,8 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#131313" "#535763"))
  '(objed-cursor-color "#ff657a")
  '(package-selected-packages
-   '(lsp-pyright dashboard python-mode pyenv-mode company-terraform terraform-doc terraform-mode lsp-jedi yapfify sphinx-doc yasnippet-snippets yasnippet-classic-snippets eslint-fix eslintd-fix bazel jenkinsfile-mode scala-mode yaml-mode js-format auto-indent-mode dockerfile-mode kotlin-mode cmake-mode cpputils-cmake tramp-theme ivy-posframe which-key wgrep-ag web-mode use-package tide smex smartparens rainbow-delimiters php-mode org-onenote org-download org-bullets multiple-cursors magit lsp-ui lsp-ivy js2-mode js-doc ivy-rich goto-chg git-timemachine expand-region exec-path-from-shell editorconfig dumb-jump drag-stuff doom-themes doom-modeline dired-subtree dired-git-info counsel-projectile company-box centaur-tabs bm anzu ag ace-window))
+   (quote
+    (all-the-icons-ivy lsp-pyright dashboard python-mode pyenv-mode company-terraform terraform-doc terraform-mode lsp-jedi yapfify sphinx-doc yasnippet-snippets yasnippet-classic-snippets eslint-fix eslintd-fix bazel jenkinsfile-mode scala-mode yaml-mode js-format auto-indent-mode dockerfile-mode kotlin-mode cmake-mode cpputils-cmake tramp-theme ivy-posframe which-key wgrep-ag web-mode use-package tide smex smartparens rainbow-delimiters php-mode org-onenote org-download org-bullets multiple-cursors magit lsp-ui lsp-ivy js2-mode js-doc ivy-rich goto-chg git-timemachine expand-region exec-path-from-shell editorconfig dumb-jump drag-stuff doom-themes doom-modeline dired-subtree dired-git-info counsel-projectile company-box centaur-tabs bm anzu ag ace-window)))
  '(pdf-view-midnight-colors (cons "#eaf2f1" "#282a3a"))
  '(rustic-ansi-faces
    ["#282a3a" "#ff657a" "#bad761" "#ffd76d" "#9cd1bb" "#9cd1bb" "#9cd1bb" "#eaf2f1"])
