30f1b6034c38da9507eeaebf71035d9e3de10673
Son Nguyen <son.nguyen9@mservice.com.vn>
2022-12-06 15:35:45 +0700

    add polybar

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,16 +10,17 @@
  '(ansi-color-names-vector
    ["#282a3a" "#ff657a" "#bad761" "#ffd76d" "#9cd1bb" "#9cd1bb" "#9cd1bb" "#eaf2f1"])
  '(custom-safe-themes
-   '("b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default))
+   '("d47f868fd34613bd1fc11721fe055f26fd163426a299d45ce69bef1f109e1e71" "266ecb1511fa3513ed7992e6cd461756a895dcc5fef2d378f165fed1c894a78c" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default))
  '(exwm-floating-border-color "#3d3f4d")
  '(fci-rule-color "#595d68")
+ '(highlight-indent-guides-method 'character)
  '(highlight-tail-colors ((("#363b3d") . 0) (("#333a46") . 20)))
  '(jdee-db-active-breakpoint-face-colors (cons "#131313" "#ffd76d"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#131313" "#bad761"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#131313" "#535763"))
  '(objed-cursor-color "#ff657a")
  '(package-selected-packages
-   '(elpy lsp-pyright dashboard python-mode pyenv-mode company-terraform terraform-doc terraform-mode yapfify sphinx-doc yasnippet-snippets yasnippet-classic-snippets eslint-fix eslintd-fix bazel jenkinsfile-mode scala-mode yaml-mode js-format auto-indent-mode dockerfile-mode kotlin-mode cmake-mode cpputils-cmake tramp-theme ivy-posframe which-key wgrep-ag web-mode use-package tide smex smartparens rainbow-delimiters php-mode org-onenote org-download org-bullets multiple-cursors magit lsp-ui lsp-ivy js2-mode js-doc ivy-rich goto-chg git-timemachine expand-region exec-path-from-shell editorconfig dumb-jump drag-stuff doom-themes doom-modeline dired-subtree dired-git-info counsel-projectile company-box centaur-tabs bm anzu ag ace-window))
+   '(counsel-etags counsel-gtags gtags-mode neotree highlight-indent-guides kubernetes-tramp kubernetes-helm kubernetes fontawesome elpy lsp-pyright dashboard python-mode pyenv-mode company-terraform terraform-doc terraform-mode yapfify sphinx-doc yasnippet-snippets yasnippet-classic-snippets eslint-fix eslintd-fix bazel jenkinsfile-mode scala-mode yaml-mode js-format auto-indent-mode dockerfile-mode kotlin-mode cmake-mode cpputils-cmake tramp-theme ivy-posframe which-key wgrep-ag web-mode use-package tide smex smartparens rainbow-delimiters php-mode org-onenote org-download org-bullets multiple-cursors magit lsp-ui lsp-ivy js2-mode js-doc ivy-rich goto-chg git-timemachine expand-region exec-path-from-shell editorconfig dumb-jump drag-stuff doom-themes doom-modeline dired-subtree dired-git-info counsel-projectile company-box centaur-tabs bm anzu ag ace-window))
  '(pdf-view-midnight-colors (cons "#eaf2f1" "#282a3a"))
  '(rustic-ansi-faces
    ["#282a3a" "#ff657a" "#bad761" "#ffd76d" "#9cd1bb" "#9cd1bb" "#9cd1bb" "#eaf2f1"])
